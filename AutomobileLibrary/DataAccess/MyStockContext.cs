3a57c08e30b078ed979cddb19ec4c6538f50f022
Dinh Tien <82441050+dinhtiendev@users.noreply.github.com>
2023-02-11 14:10:15 +0700

    Add project files.

---
+++ AutomobileLibrary/DataAccess/MyStockContext.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.Extensions.Configuration;
+
+namespace AutomobileLibrary.DataAccess
+{
+    public partial class MyStockContext : DbContext
+    {
+        public MyStockContext()
+        {
+        }
+
+        public MyStockContext(DbContextOptions<MyStockContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Car> Cars { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var config = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
+            optionsBuilder.UseSqlServer(config.GetConnectionString("MyStock"));
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Car>(entity =>
+            {
+                entity.HasKey(e => e.CarId)
+                    .HasName("PK__Car__51BCD797F8A376FA");
+
+                entity.ToTable("Car");
+
+                entity.Property(e => e.CarId)
+                    .ValueGeneratedNever()
+                    .HasColumnName("CarId");
+
+                entity.Property(e => e.CarName)
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Manufacturer)
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Price).HasColumnType("money");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
